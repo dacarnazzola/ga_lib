3cfcb90beeca2af832367760d1b845241fefd255
Derick Carnazzola <maxediv@gmail.com>
2026-03-29 00:41:20 -0500

    fixed test random bug

--- src/tests/test_random.f90
+++ src/tests/test_random.f90
@@ -1,5 +1,5 @@
 program test_random
-use, non_intrinsic :: kinds, only: stdout, i32, sp
+use, non_intrinsic :: kinds, only: stdout, i32, i64, dp
 use, non_intrinsic :: random, only: random_uniform_i32
 implicit none
 
@@ -7,11 +7,11 @@ implicit none
 
     integer(kind=i32) :: x(n), i
 
-    call random_uniform_i32(x, n, xlo, xhi)
+    call random_uniform_i32(x, size(x, kind=i32), xlo, xhi)
 
     !$omp parallel do
     do i=xlo,xhi
-        write(unit=stdout, fmt='(a,i0,a,f0.1,a)') 'x==',i,' (',count(x==i)*100.0_sp/n,'%)'
+        write(unit=stdout, fmt='(a,i0,a,f0.1,a)') 'x==',i,' (',count(x==i, kind=i64)*100.0_dp/size(x, kind=i64),'%)'
     end do
     !$omp end parallel do
 
